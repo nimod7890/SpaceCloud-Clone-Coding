1cf7cb6eec93e710f2dd160fb4605da4b74e0673
nimod7890 <nimod7890@naver.com>
2021-08-22 22:25:04 +0900

    Seqeulize로 바뀐 코드 정리

--- main.js
+++ main.js
@@ -1,36 +1,90 @@
-var express = require('express');
-var path = require('path');
+#!/usr/bin/env node
 
-var indexRouter = require('./backend/routes/index');
-var usersRouter = require('./backend/routes/users');
-var homeRouter = require('./backend/routes/home');
-const app = express();
+/**
+ * Module dependencies.
+ */
 
-app.set('views', path.join(__dirname, 'backend/views'));
-app.set('view engine', 'pug');
-app.set('port', process.env.PORT || 3000);
+var app = require('./backend/app');
+var debug = require('debug')('backend:server');
+var http = require('http');
 
-app.use(express.json());
-app.use(express.urlencoded({ extended: false }));
+/**
+ * Get port from environment and store in Express.
+ */
 
+var port = normalizePort(process.env.PORT || '3000');
+app.set('port', port);
 
-app.use('/', indexRouter);
-app.use('/users', usersRouter);
-app.use('/home', homeRouter)
+/**
+ * Create HTTP server.
+ */
 
+var server = http.createServer(app);
 
+/**
+ * Listen on provided port, on all network interfaces.
+ */
 
-// error handler
-app.use(function(err, req, res, next) {
-  // set locals, only providing error in development
-  res.locals.message = err.message;
-  res.locals.error = req.app.get('env') === 'development' ? err : {};
+server.listen(port);
+server.on('error', onError);
+server.on('listening', onListening);
 
-  // render the error page
-  res.status(err.status || 500);
-  res.render('error');
-});
+/**
+ * Normalize a port into a number, string, or false.
+ */
 
-app.listen(app.get('port'), () => {
-  console.log('Express server listening on port ' + app.get('port'));
-});
+function normalizePort(val) {
+  var port = parseInt(val, 10);
+
+  if (isNaN(port)) {
+    // named pipe
+    return val;
+  }
+
+  if (port >= 0) {
+    // port number
+    return port;
+  }
+
+  return false;
+}
+
+/**
+ * Event listener for HTTP server "error" event.
+ */
+
+function onError(error) {
+  if (error.syscall !== 'listen') {
+    throw error;
+  }
+
+  var bind = typeof port === 'string'
+    ? 'Pipe ' + port
+    : 'Port ' + port;
+
+  // handle specific listen errors with friendly messages
+  switch (error.code) {
+    case 'EACCES':
+      console.error(bind + ' requires elevated privileges');
+      process.exit(1);
+      break;
+    case 'EADDRINUSE':
+      console.error(bind + ' is already in use');
+      process.exit(1);
+      break;
+    default:
+      throw error;
+  }
+}
+
+/**
+ * Event listener for HTTP server "listening" event.
+ */
+
+function onListening() {
+  var addr = server.address();
+  var bind = typeof addr === 'string'
+    ? 'pipe ' + addr
+    : 'port ' + addr.port;
+  debug('Listening on ' + bind);
+}
